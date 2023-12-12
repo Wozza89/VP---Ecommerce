f7bf99a66550bbf5b9abee2dbf041a8bf6092afb
Warren Pitterson <warrenpitterson@gmail.com>
2023-12-12 00:56:32 +0000

    program filed added and sqlServer Package Added

--- VictoriaPlumbing-Ecommerce/Program.cs
+++ VictoriaPlumbing-Ecommerce/Program.cs
@@ -1,6 +1,21 @@
+using Core.Services;
+using Microsoft.EntityFrameworkCore;
+using Victoria.Plumbing.Data;
+using VictoriaPlumbing.Core.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddDbContext<DataDbContext>(builder =>
+{
+    builder.UseSqlServer("Server=(localdb)\\ProjectModels;Database=VP_Orders;"); //wp - hardcoded move to settings if time allows 
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
