0a5a10e6304af6cd5a3e281777271f1de5ba7e1f
Warren Pitterson <warrenpitterson@gmail.com>
2023-12-12 00:34:37 +0000

    DataDbContext Added and Model Project reference added to Data Project

---
+++ Victoria.Plumbing.Data/DataDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Victoria.Plumbing.Models.DomainModels;
+
+namespace Victoria.Plumbing.Data
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Orders>().HasKey(orders => orders.Id);
+            modelBuilder.Entity<Orders>().HasOne(orders => orders.Customer);
+            modelBuilder.Entity<Product>().HasKey(product => product.Id);
+            modelBuilder.Entity<Orders>().HasKey(orders => orders.Id);
+            modelBuilder.Entity<OrderItem>().HasKey(orders => orders.Id);
+        }
+    }
+}
+}
